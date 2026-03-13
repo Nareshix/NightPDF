cfdd223baaf35d149bb21b1021daabece3f52bfc
Nareshix <Nareshix66@gmail.com>
2026-03-14 00:26:32 +0800

    changed name

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use viewer::PdfViewer;
 fn main() -> eframe::Result<()> {
     env_logger::init();
     eframe::run_native(
-        "PDF Dark Reader",
+        "NightPDF",
         eframe::NativeOptions {
             viewport: eframe::egui::ViewportBuilder::default()
                 .with_inner_size([1000.0, 860.0])
                 .with_min_inner_size([600.0, 400.0])
-                .with_title("PDF Dark Reader"),
+                .with_title("NightPDF"),
             ..Default::default()
         },
         Box::new(|_cc| Ok(Box::new(PdfViewer::new()))),
